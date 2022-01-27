7401c882e373cc0a07df40a5686aa1fc37d7b7a3
Jamie Bunch <JamieBunch@ymail.com>
2022-01-27 12:15:59 -0500

    add second restaurant

--- index.js
+++ index.js
@@ -2,12 +2,27 @@
 const restaurants = require('./restaurants.json');
 
 // import a set of tools to talk to firebasw and firestore
-const{ initializeApp, applicationDefaultt, cert} = require('firebase-admin');
+const{ initializeApp, applicationDefaultt, cert} = require('firebase-admin/app');
 const{ getFirestore, Timestamp, FieldValue} = require('firebase-admin/firestore');
 
-// connect to Firestore
+// import our credentials
 const credentials = require('./credentials.json');
+
+// connect to firebase services
+initializeApp({
+    credential: cert(credentials)
+});
+
+// connect to Firestore
+const db = getFirestore();
 
 // create a collection called "restaurants"
 
 // add each restaurant
+db.collection('restaurants').add(restaurants[1])
+.then(doc => {
+    console.log('Added restaurants', doc.id);
+})
+.catch(err => {
+    console.error(err);
+})
