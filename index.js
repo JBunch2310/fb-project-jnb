c03744c6cdccc6be1c79d53ccdb0821247f66847
Jamie Bunch <JamieBunch@ymail.com>
2022-01-27 11:46:00 -0500

    add credentials

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // import our restaurants
 const restaurants = require('./restaurants.json');
 
-console.log(restaurants[0].name)
+// import a set of tools to talk to firebasw and firestore
+const{ initializeApp, applicationDefaultt, cert} = require('firebase-admin');
+const{ getFirestore, Timestamp, FieldValue} = require('firebase-admin/firestore');
 
 // connect to Firestore
+const credentials = require('./credentials.json');
 
 // create a collection called "restaurants"
 
